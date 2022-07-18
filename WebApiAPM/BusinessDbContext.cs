d8cf0065075f79613cb692daac815fe316fedf0f
Benjamin Correa <benjamin.c@smartwaystudio.com>
2022-07-18 12:04:40 -0300

    add ef core

---
+++ WebApiAPM/BusinessDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApiAPM.Entities;
+
+namespace WebApiAPM
+{
+    public class BusinessDbContext : DbContext
+    {
+        public BusinessDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ WebApiAPM/Entities/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApiAPM.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Stock { get; set; }
+    }
+}
